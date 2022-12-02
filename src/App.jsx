acf20dab1aeac0903e36561855acd695d900376b
Ben Vanderhoff <88126013+RandomUserWithInternet@users.noreply.github.com>
2022-12-02 15:29:07 -0800

    Got the day box working

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
+  
+  const dayOf = {
+    month: '1',
+    week: 'Monday',
+};
+
   return (
-    <div className="App">
-      <Day />
+    <div>
+      <div className="App">
+        <Day dayOf={dayOf} />
+      </div>
     </div>
   )
 }
 
+const Day = (props) => {
+  const dayOf = props.dayOf;
 
-function Day(){
   return (
     <center>
-      <div className="border-4 box-border border-black h-24 w-32">day #</div>
+      <div className="border-4 box-border border-black h-24 w-32">
+        <h1>{dayOf.month}</h1>
+      </div>
     </center>
-  )
+  );
 }
 export default App
