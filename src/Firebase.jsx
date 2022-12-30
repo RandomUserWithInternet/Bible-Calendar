1895d35d122fc3068383466ce58017c45895e133
Ben Vanderhoff <88126013+RandomUserWithInternet@users.noreply.github.com>
2022-12-30 14:37:53 -0800

    Working on the user settings

--- src/Firebase.jsx
+++ src/Firebase.jsx
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { doc, getFirestore, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
--- src/UserSettings.jsx
+++ src/UserSettings.jsx
@@ -5,31 +5,19 @@ import { useState } from 'react';
 
 export default function UserSettings() {
     const [name, setName] = useState('');
-    const [pfp, setPfp] = useState(null);
-
-    function handleSubmit(e) {
-        e.preventDefault();
-        const userInfo = {name, pfp};
-        saveUserInfo(userInfo);
-    }
+    const [pfp, setPfp] = useState(undefined);
 
     return(
     <Popup trigger={<button className="button"> <img className="h-12 w-12"src="https://img.icons8.com/material-sharp/512/user.png"/></button>} modal>
         <div id='pfp' className='border-2 border-black'>
-            <form onSubmit={handleSubmit}>
-                <label>Image:</label>
-                <input type="file" value={pfp} onChange={(e)=>setPfp(e.target.value)} required /><br/>
-                <label>Name:</label>
-                <input type="text" value={name} onChange={(e)=>setName(e.target.value)} required /><br/>
-                <input type="submit" value="Submit"/>
-            </form>
-
-            
+            <input type="file" value={pfp} onChange={(e)=>setPfp(e.target.files[0])} accept="image/jpeg, image/png" />
+            <input type="text" value={name} onChange={(e)=>setName(e.target.value)} />
+            <button onClick={saveUserInfo(name, pfp)}>Submit O:</button>
         </div>
     </Popup>
     );
   }
 
-function saveUserInfo(info) {
-    
+function saveUserInfo(name, pfp) {
+    console.log(name);
 }
