14defe996a3cdaebcad67ad451ff56df7a5eb909
Ben Vanderhoff <88126013+RandomUserWithInternet@users.noreply.github.com>
2023-01-04 15:05:58 -0800

    adddoc was causing problems. temp commenting out

--- src/UserSettings.jsx
+++ src/UserSettings.jsx
@@ -1,6 +1,6 @@
 import Popup from 'reactjs-popup';
 import { doc, setDoc } from 'firebase/firestore';
-import { auth } from "./Firebase"
+import { auth, db } from "./Firebase"
 import { useState } from 'react';
 
 export default function UserSettings() {
@@ -12,12 +12,20 @@ export default function UserSettings() {
         <div id='pfp' className='border-2 border-black'>
             <input type="file" value={pfp} onChange={(e)=>setPfp(e.target.files[0])} accept="image/jpeg, image/png" />
             <input type="text" value={name} onChange={(e)=>setName(e.target.value)} />
-            <button onClick={saveUserInfo(name, pfp)}>Submit O:</button>
+            {/* <button onClick={saveUserInfo(name, pfp)}>Submit O:</button> */}
         </div>
     </Popup>
     );
   }
 
-function saveUserInfo(name, pfp) {
-    console.log(name);
-}
+// async function saveUserInfo(name, pfp) {
+//     try {
+//         const docRef = await addDoc(collection(db, "Users"), {
+//           name: sldkfj
+//         });
+      
+//         console.log("Document written with ID: ", docRef.id);
+//       } catch (e) {
+//         console.error("Error adding document: ", e);
+//       }
+// }
